f30f98cef80a1afe103cf9473a1fd11d5774b692
Filip Peterek <fpeterek@seznam.cz>
2020-08-24 19:57:10 +0200

    Feedback messages

--- carcan/server.py
+++ carcan/server.py
@@ -4,23 +4,56 @@ import os
 from carinterface import CarInterface
 
 
+debug = os.getenv('SOCKET_DEBUG') and os.getenv('SOCKET_DEBUG').lower() in ('true', '1')
+
+
 class Server:
 
     _car: CarInterface = None
 
     class Handler(socketserver.BaseRequestHandler):
 
-        def handle(self):
-            data = self.request.recv(2)
-            v = int.from_bytes(data[0:1], 'little', signed=True)
-            s = int.from_bytes(data[1:2], 'little', signed=True)
-
-            print(f'Received (v, s) = ({v}, {s})')
+        def drive(self, v, s):
+            if debug:
+                print(f'Received (v, s) = ({v}, {s})')
 
             if Server._car is not None:
                 Server._car.drive(v, s)
 
-            self.request.sendall((1).to_bytes(1, 'little'))
+            self.healthcheck()
+
+        def healthcheck(self):
+            if debug:
+                status = Server._car.is_ok if Server._car is not None else False
+                print(f'Healthcheck (status={("Dead", "Alive")[status]})')
+
+            status = int(Server._car.is_ok if Server._car is not None else False)
+            self.request.sendall(status.to_bytes(1, 'little'))
+
+        def info(self):
+
+            v = Server._car.velocity if Server._car is not None else 0
+            s = Server._car.steering_angle if Server._car is not None else 0
+
+            if debug:
+                print(f'Driving data (v, s) = ({v}, {s})')
+
+            v = v.to_bytes(1, 'little')
+            s = s.to_bytes(1, 'little')
+            self.request.sendall(v + s)
+
+        def handle(self):
+            data = self.request.recv(3)
+            t = int.from_bytes(data[0:1], 'little', signed=False)
+            v = int.from_bytes(data[1:2], 'little', signed=True)
+            s = int.from_bytes(data[2:3], 'little', signed=True)
+
+            if t == 0:
+                self.drive(v, s)
+            elif t == 1:
+                self.healthcheck()
+            elif t == 2:
+                self.info()
 
     @staticmethod
     def serve(car: CarInterface = None, host: str = None, port: int = None):
--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export CAN_PRINTER=0
-export CAN_DEBUG=1
+export CAN_DEBUG=0
+export SOCKET_DEBUG=1
 export SERVER_HOST=localhost
 export SERVER_PORT=8080
